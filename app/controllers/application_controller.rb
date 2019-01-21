f5a5141f4f430d6ac4db7d5cb0b523bcc9c67fc3
luongtran <luongtranduc@gmail.com>
2019-01-21 23:16:48 +0700

    fix style of welcome language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include LocaleDefinition
   protect_from_forgery with: :exception
   helper_method :current_chef, :logged_in?
   before_action :configure_permitted_parameters, if: :devise_controller?
