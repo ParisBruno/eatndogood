375b11c22684a57766aa4256421db58194cda1b5
luongtran <luongtranduc@gmail.com>
2019-02-09 16:36:00 +0700

    fix edit page, remove icons on edit box

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -4,16 +4,11 @@ CKEDITOR.editorConfig = function (config) {
 
     config.toolbar_Easy =
     [
-        ['Source','-','Preview'],
-        ['Cut','Copy','Paste','PasteText','PasteFromWord',],
-        ['Undo','Redo','-','SelectAll','RemoveFormat'],
-        ['Styles','Format'],['Maximize','-','About'],
-        ['Subscript', 'Superscript', 'TextColor'],
-        ['Bold','Italic','Underline','Strike'], ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-        ['Link','Unlink','Anchor'],
-      /*   ['Image', 'Attachment', 'Flash', 'Embed'], */
-        ['Table','HorizontalRule','Smiley','SpecialChar','PageBreak']
+        ['Undo','Redo','-','RemoveFormat'],
+        ['Styles','Format'],
+        ['TextColor'],
+        ['Bold','Italic','Underline'], 
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock']
     ];
 
 }
