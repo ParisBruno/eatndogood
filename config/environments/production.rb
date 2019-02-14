5640276f9801b016ebb8e6755e4c59f4441e67ee
luongtran <luongtranduc@gmail.com>
2019-02-15 00:25:54 +0700

    fixing sending email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "myrecipes_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.stackmail.com',
+    port:                 587,
+    domain:               'itoprecipes.com',
+    user_name:            'no-reply@itoprecipes.com',
+    password:             'Vl149d439',
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
