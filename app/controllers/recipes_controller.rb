03485c19817b3f60c57c6aa1de049453c3c1a22d
dimonrud911 <dimon.rudenskyi@gmail.com>
2021-05-17 14:23:10 +0300

    Recipes -> index -> allow-access-without-login

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   respond_to :html, :js
-  before_action :require_logged_in
+  before_action :require_logged_in, except: [:index]
   before_action :set_recipe, only: [:show, :edit, :update, :destroy, :like]
   before_action :require_user, except: [:index, :show, :like]
   before_action :require_same_user, only: [:edit, :update, :destroy]
