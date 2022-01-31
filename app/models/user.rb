7d61336c4577cce5a2c4c54b1986947e354bcee4
dimonrud911 <dimon.rudenskyi@gmail.com>
2022-02-01 01:39:42 +0200

    create-app error

--- app/models/user.rb
+++ app/models/user.rb
@@ -95,7 +95,7 @@ class User < ApplicationRecord
   end
 
   def check_managers_count
-    managers = app.users.where(manager: true)
+    managers = app&.users&.where(manager: true)
     if (new_record? && manager?) || (manager? && managers.exclude?(self))
       errors.add(:manager, "- you can't create more than 4 managers") if managers.count >= 4
     end
