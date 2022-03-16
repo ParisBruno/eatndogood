9be563258cc006d7400caf6417196001722092da
kanchan <kanchankamboj1020@gmail.com>
2022-03-16 16:30:15 +0530

    Staging setup

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,4 +1,4 @@
-if Rails.env.development?
+if Rails.env.development? || Rails.env.staging?
 	Rails.configuration.paypal = {
 		client_id: "AQK4qrPRygRU-GhNrHHxXlcCoZQfsdPvmKnquVFyEJJJu72gmijkaJn5H-5TjiyEtPcKU-hWsGCdOwyC",
 		client_secret: "EOdkwiKgMTDNQG_D10pwYDQgZmg6D2uQ1wGHlCXUz4gCwcQlfy5MXZzbZP9kw31cv2eqDlqBsrQj_9Gj",
