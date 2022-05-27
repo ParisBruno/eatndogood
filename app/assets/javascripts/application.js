81202d1aa5148d214b78a49c394eab37c49cee8a
kanchan <kanchankamboj1020@gmail.com>
2022-05-27 12:38:25 +0530

    Make js method common

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -98,3 +98,21 @@ $(function() {
 $(function() {
   $('.style-sortable, .ingredient-sortable, .allergen-sortable').railsSortable();
 });
+
+$(document).ready(function(){
+  $( ".collapsible-item-title-link" ).each(function() {
+    var icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16"> <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/> </svg>'
+    if ($(this).find('span')[0] == undefined) {
+      var color = '#007bff';
+    } else {
+      var eleColor = $(this).find('span')[0].style.color;
+      if (eleColor == '') {
+        var color = '#007bff';
+      } else {
+        var color = eleColor;
+      }
+    }
+    $(this).attr('style', 'color:'+ color + '!important');
+    $( this ).append(icon);
+  });
+});
