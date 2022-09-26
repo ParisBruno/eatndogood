41e2f6a7b4331c7992608df0cad4ae3f56f53b49
kanchan <kanchankamboj1020@gmail.com>
2022-09-26 19:05:20 +0530

    Comment aspect ratio validations

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,10 +7,10 @@ class Page < ActiveRecord::Base
   # has_attached_file :page_img, styles: { large: "900x600>", thumb: "150x100>" }
   has_one_attached :page_img
   has_one_attached :landscape_page_img
-  validates :landscape_page_img, blob: { content_type: %w(image/png image/jpeg image/jpg image/gif)}, unless: -> { landscape_page_img.blank? }, aspect_ratio: :landscape
+  validates :landscape_page_img, blob: { content_type: %w(image/png image/jpeg image/jpg image/gif)}, unless: -> { landscape_page_img.blank? }
 
   validates :page_img, blob: { content_type: %w(image/png image/jpeg image/jpg image/gif)}, unless: -> { page_img.blank? }
-  validates :page_img, aspect_ratio: :portrait, if: -> { destination == 'welcome' }
+  # validates :page_img, aspect_ratio: :portrait, if: -> { destination == 'welcome' }
   # validates_attachment_content_type :page_img, content_type: /\Aimage\/.*\z/
   has_one :page_preview, dependent: :destroy
 
@@ -27,7 +27,6 @@ class Page < ActiveRecord::Base
   end
 
   def self.hide(_)
-    debugger
   end
 
   def self.sizes
